0744ac1a54f1ff5330ef63dfcddfcbf7f2a9f7cb
Sergio Martin <alu-sermarvar@foc.es>
2024-11-20 18:05:38 +0100

    añadido buscar_palabra.py

---
+++ PCAP/Programas/buscar_palabra.py
@@ -0,0 +1,14 @@
+palabra = input("Introduce una palabra a buscar ").upper()
+grupo = input("Introduce grupo de caracteres ").upper()
+
+contiene = True
+posicion = 0
+for c in palabra:
+    posicion = grupo.find(c)
+    if posicion < 0:
+        contiene = False
+        break;
+    else:
+        continue
+print(contiene)
+        
