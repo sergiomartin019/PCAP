ffca846ad0aa6e4df7a678807d26f9332ad884fe
Sergio Martin <alu-sermarvar@foc.es>
2024-11-20 17:44:21 +0100

    actualizado numerovital.py

--- PCAP/Programas/numerovital.py
+++ PCAP/Programas/numerovital.py
@@ -1,18 +1,19 @@
 fecha = ""
 while True:
     fecha = input("Ingrese la fecha de nacimiento (AAAA/MM/DD): ")
-    if fecha.isnumeric():
+    if fecha.isnumeric() and len(fecha) == 8:
         break;
     
 digito = 0
 suma = 0;
 
 for char in fecha:
-    digito = int(char)
-    suma = suma + digito
-suma2 = 0
-for char in str(suma):
-    digito = int(char)
-    suma2 = suma2 + digito
-print("sos el", suma2)
+    suma += int(char)
+if len(str(suma)) > 1:
+    for c in str(suma):
+        digito = digito + int(c)
+else:
+    digito = suma
+print("el digito es", digito)
+
     
